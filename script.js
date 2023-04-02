81e098a1487c1f0c76a856b2380be6cca23e7db7
simer <simerpreet2844@gmail.com>
2023-04-02 12:21:52 -0400

    adding responsiveness menu size smaller

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenav").style.width = "200px";
   }
   
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
 
+  //  video play
 
     document.getElementById('vid').play();
 
+    // swipper
     var swiper = new Swiper(".mySwiper", {
         loop: true,
         slidesPerView: 4,
@@ -24,6 +26,7 @@ function openNav() {
         },
       });
 
+      // for active navbar 
 
       document.querySelectorAll(".nav-items").forEach((ele) =>
   ele.addEventListener("click", function (event) {
